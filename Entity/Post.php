69783c3a8225fc2d8b0887ea5e3f0db59d191ae1
Cecile Salon <cecile.salon@aropixel.com>
2023-10-02 15:45:28 +0200

    Bugfix post setId

--- Entity/Post.php
+++ Entity/Post.php
@@ -107,6 +107,11 @@ class Post implements PostInterface
         return $this->id;
     }
 
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
     public function getStatus(): ?string
     {
         return $this->status;
